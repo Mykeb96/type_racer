d737988374ef03b0c935183db79017a48ea48c0c
unknown <mykael.barnes@hotmail.com>
2025-10-14 16:59:37 -0700

    feat(App): setting up initial App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 
 import './styles/App.css'
 
+import { useState } from 'react'
+
+const initialParagraph: string = `The quick brown fox wasn't just fast—it was clever. 
+As it darted through the forest, leaves scattered beneath its paws. Each step echoed 
+with purpose, every motion a blur of focus and grace. To chase it was to test your own limits.`
+
 function App() {
+  const [paragraph, setParagraph] = useState<string>(initialParagraph)
 
   return (
-    <>
-     
-    </>
+    <div id="App">
+      <div className='paragraph_container'>
+        {paragraph}
+      </div>
+    </div>
   )
 }
 
