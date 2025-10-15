12855c847f2913b3abf720881472e72843520940
unknown <mykael.barnes@hotmail.com>
2025-10-14 17:27:24 -0700

    feat(input): added input detection for onTrack state

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,50 @@
 
-import './styles/App.css'
+import './styles/App.css';
 
-import { useState } from 'react'
+import { useState, useEffect } from 'react';
 
-const initialParagraph: string = `The quick brown fox wasn't just fast—it was clever. 
-As it darted through the forest, leaves scattered beneath its paws. Each step echoed 
-with purpose, every motion a blur of focus and grace. To chase it was to test your own limits.`
+interface gameState {
+  roundStarted: boolean,
+  onTrack: boolean
+}
+
+const initialGameState: gameState = {
+  roundStarted: true,
+  onTrack: true
+}
+
+const initialParagraph: string = `The quick brown fox wasn't just fast - it was clever. As it darted through the forest, leaves scattered beneath its paws. Each step echoed with purpose, every motion a blur of focus and grace. To chase it was to test your own limits.`;
 
 function App() {
-  const [paragraph, setParagraph] = useState<string>(initialParagraph)
+  const [gameState, setGameState] = useState<gameState>(initialGameState);
+  const [paragraph, setParagraph] = useState<string>(initialParagraph);
+  const [userInput, setUserInput] = useState<string>('')
+
+  const handleInput = (e: any) => {
+    setUserInput(e.target.value)
+  }
+
+  useEffect(() => {
+    if (gameState.roundStarted) {
+      let paragraphSubStr = paragraph.substr(0, userInput.length);
+      if (userInput === paragraphSubStr) {
+        console.log('good')
+      } else {
+        console.log('bad')
+      }
+    }
+  }, [userInput])
 
   return (
     <div id="App">
-      <div className='paragraph_container'>
-        {paragraph}
+      <div id="Container">
+        <div className="Paragraph_Container">
+          <p className="Paragraph_Text">{paragraph}</p>
+        </div>
+
+        <div className="Input_Container">
+          <input disabled={!gameState.roundStarted} onChange={(e: any) => handleInput(e)} type="text" className="User_Input"/>
+        </div>
       </div>
     </div>
   )
